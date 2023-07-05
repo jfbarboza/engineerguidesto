1a67b9e4111d6fc9b2f201d65a5cf35b8d9fe93b
Jorge <barbozajorge@gmail.com>
2023-07-05 17:31:44 -0400

    Adding button component

--- components/events/event-item.tsx
+++ components/events/event-item.tsx
@@ -1,6 +1,10 @@
 import { EventItemType } from '../../core/types';
 import Link from 'next/link';
 import classes from './event-item.module.css';
+import Button from '../ui/button';
+import DateIcon from '../icons/date-icon';
+import AddressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
 
 function EventItem(props: EventItemType) {
     const { 
@@ -24,14 +28,16 @@ function EventItem(props: EventItemType) {
                 <div className={classes.summary}>
                     <h2>{title}</h2>
                     <div className={classes.date}>
+                        <DateIcon />
                         <time>{ humanReadableDate }</time>
                     </div>
                     <div className={classes.address}>
+                        <AddressIcon />
                         <address>{ formattedAddress }</address>
                     </div>
                 </div>
-                <div className={classes.action}>
-                    <Link href="{ exploreLink }">Explore Event</Link>
+                <div className={classes.actions}>
+                    <Button link="{ exploreLink }"><span>Explore Event</span><span className={classes.icon}><ArrowRightIcon/></span></Button>
                 </div>
             </div>
         </li>
