80ef57ff08b3762fec952e2c42c51c615b2de12e
Jorge <barbozajorge@gmail.com>
2023-07-07 17:41:31 -0400

    Adding fun image to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,14 @@ export default function Home() {
   return (
     <>
       <section className={classes.main}>
-        <div>
-          <h1>The guide books for smart people.</h1>
-          <Button link="/books" color="blue">Browse Books</Button>
+        <div className={classes.content}>
+          <div className={classes.image}>
+            <img src="/images/smart-people.png" alt="smart people" />
+          </div>
+          <div>
+            <h1>The guide books for <span className={classes.underline}>smart</span> people.</h1>
+            <Button link="/books" color="blue">Browse Books</Button>
+          </div>
         </div>
         
       </section>
