a18cc90e4689b84c474597727066cabb84358ad8
Jorge <barbozajorge@gmail.com>
2023-07-10 08:39:37 -0400

    Books Slider with custom arrows

--- components/books/books-carousel/books-carousel.tsx
+++ components/books/books-carousel/books-carousel.tsx
@@ -3,6 +3,34 @@ import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import CarouselItem from './carousel-item';
 import classes from './books-carousel.module.css';
+import { Fragment } from 'react';
+
+interface CustomLeftArrowProps extends ArrowProps {
+  myOwnStuff: string
+}
+interface CustomRightArrowProps extends ArrowProps {
+  myOwnStuff: string
+}
+const CustomLeftArrow = ({ onClick }: CustomLeftArrowProps) => {
+  return (
+    <button 
+      aria-label="Go to next slide"
+      onClick={onClick} className={classes.left_arrow}>
+      
+    </button>
+  )
+}
+
+const CustomRightArrow = ({ onClick }: CustomRightArrowProps) => {
+  return (
+    <button 
+      aria-label="Go to next slide"
+      onClick={onClick} className={classes.right_arrow}>
+      
+    </button>
+  )
+}
+
 function BooksCarousel( props: any){
     const { books } = props;
     console.log(`books: ${books}`);
@@ -26,22 +54,26 @@ function BooksCarousel( props: any){
         }
       };
       return(
-        <Carousel 
-          responsive={responsive}
-          draggable={false}
-          showDots={true}
-          infinite={true}
-          itemClass={classes.carousel}
-          >
-            { books.map((book: any) => (
-                <CarouselItem
-                    image={book.cover}
-                    id={book.id}
-                    title={book.title}
-                    subtitle={book.subtitle}
-                    key={book.id}/>
-            ))}
-        </Carousel>
+        <Fragment>
+          <h2 className={classes.title}>Our Books</h2>
+          <Carousel 
+            responsive={responsive}
+            draggable={false}
+            infinite={true}
+            itemClass={classes.carousel}
+            customRightArrow={<CustomRightArrow />}
+            customLeftArrow={<CustomLeftArrow />}
+            >
+              { books.map((book: any) => (
+                  <CarouselItem
+                      image={book.cover}
+                      id={book.id}
+                      title={book.title}
+                      subtitle={book.subtitle}
+                      key={book.id}/>
+              ))}
+          </Carousel>
+        </Fragment>
       );
 }
 
